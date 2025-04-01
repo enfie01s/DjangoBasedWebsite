347bfa8922d1be6fc107e64c6d38e12d7c0ac302
Stella Enfield <enfie01s@users.noreply.github.com>
2025-04-01 14:19:38 +0100

    Add urls for mat and stella pages

--- sitepages/urls.py
+++ sitepages/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     #url(r'^create/', views.create, name='create'), 
     url(r'^$', views.home, name='home'),
     url(r'^homepage/$', views.homepage, name='homepage'),
+    url(r'^mat/$', views.mat, name='mat'),
+    url(r'^stella/$', views.stella, name='stella'),
 ]
